25b00562505cc04ac939856ddc08c0ef3bad08ac
cargon21 <63457603+cargon21@users.noreply.github.com>
2022-03-10 11:53:47 -0500

    New Problem

--- lastWordLength.py
+++
@@ -1,8 +0,0 @@
-def lengthOfLastWord(s: str) -> int:
-    return 0
-
-print(lengthOfLastWord("The cow jumped over the moon") == 4)
-print(lengthOfLastWord("Hello, world   ") == 5)
-print(lengthOfLastWord("SUCSC is the best club at Suffolk") == 7)
-print(lengthOfLastWord("Hey") == 3)
-print(lengthOfLastWord("   Last test, good luck   ") == 4)
