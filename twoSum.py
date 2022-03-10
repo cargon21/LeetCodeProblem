726163ab0f16a911167ac7a0cd83e7e1b110a4b0
cargon21 <63457603+cargon21@users.noreply.github.com>
2022-03-10 11:54:29 -0500

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -2,9 +2,9 @@ def twoSum(nums, target):
     return [0,0]
 
 def main():
-    print( twoSum([2,7,11,15], 9) == [0,1])
-    print( twoSum([3,2,4], 6) == [1,2])
-    print( twoSum([3,3], 6) == [0,1])
+    print(twoSum([2,7,11,15], 9) == [0,1])
+    print(twoSum([3,2,4], 6) == [1,2])
+    print(twoSum([3,3], 6) == [0,1])
 
 if __name__ == "__main__":
     main()
